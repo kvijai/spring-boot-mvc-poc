fd5d4f9aa4dd680028e05895ae8e42453819728c
kvijai <kvijai.iitk@gmail.com>
2018-11-12 19:36:13 +0530

    Initial commit

---
+++ src/main/java/com/vj/poc/mvc/springbootmvcpoc/SpringBootMvcPocApplication.java
@@ -0,0 +1,12 @@
+package com.vj.poc.mvc.springbootmvcpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcPocApplication.class, args);
+	}
+}
